10e0a81dbca7349ad6c40ea528e17f16528d66bb
Benny Thadikaran <tbenny05@yahoo.com>
2024-03-21 15:59:26 +0530

    added def to check if pandas DatetimeIndex is intraday

--- src/utils.py
+++ src/utils.py
@@ -63,6 +63,18 @@ def make_serializable(obj: T) -> T:
     return serialize(obj)
 
 
+def has_time_component(datetime_index: pd.DatetimeIndex) -> bool:
+    """Return True if any value in DatetimeIndex has time component
+    other than `00:00:00` (Midnight hour)
+    Ex Datetime(2023, 12, 10)           ->  `00:00:00` (Defaults to midnight)
+       Datetime(2023, 12, 10, 0, 0)     ->  `00:00:00` (Midnight time)
+       Datetime(2023, 12, 10, 12, 10)   ->  `12:10:00`
+    """
+    return any(
+        datetime_index.to_series().dt.time != pd.Timestamp("00:00:00").time()
+    )
+
+
 def get_atr(
     high: pd.Series, low: pd.Series, close: pd.Series, window=15
 ) -> pd.Series:
