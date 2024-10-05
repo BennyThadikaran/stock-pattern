5946d2aee7ec5a1e032ff0ac8c8c054ff6e0d3a3
Benny Thadikaran <tbenny05@yahoo.com>
2024-10-05 19:42:03 +0530

    Bump to version 3.2.2

--- src/init.py
+++ src/init.py
@@ -286,7 +286,7 @@ def process(
 # Differentiate between the main thread and child threads on Windows
 # see https://stackoverflow.com/a/57811249
 if __name__ == "__main__":
-    version = "3.2.1"
+    version = "3.2.2"
 
     futures: List[concurrent.futures.Future] = []
 
