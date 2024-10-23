3b6b23a4bb403b3a44dbeafb6d0604f049de2292
Benny Thadikaran <tbenny05@yahoo.com>
2024-10-23 12:27:03 +0530

    Changed FIB extension calculation in find AB=CD functions.

--- src/utils.py
+++ src/utils.py
@@ -1557,7 +1557,7 @@ def find_bullish_abcd(
         c_fib_inverse = 1 / c_nearest_fib
 
         bc_extension = c - ab_diff
-        bc_fib_extension = c - (bc_diff * c_fib_inverse)
+        bc_fib_extension = c - (ab_diff * c_fib_inverse)
 
         terminal_point = min(bc_extension, bc_fib_extension)
 
@@ -1687,7 +1687,7 @@ def find_bearish_abcd(
         c_fib_inverse = 1 / c_nearest_fib
 
         bc_extension = c + ab_diff
-        bc_fib_extension = c + (bc_diff * c_fib_inverse)
+        bc_fib_extension = c + (ab_diff * c_fib_inverse)
 
         terminal_point = max(bc_extension, bc_fib_extension)
 
