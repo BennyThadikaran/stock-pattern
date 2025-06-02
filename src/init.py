baa2cfb7828139fb1945a30d9e6eeab0cf9b4eb9
Benny Thadikaran <tbenny05@yahoo.com>
2025-06-02 18:32:15 +0530

    bump to version 4.0.10

--- src/init.py
+++ src/init.py
@@ -338,7 +338,7 @@ def process(
 # Differentiate between the main thread and child threads on Windows
 # see https://stackoverflow.com/a/57811249
 if __name__ == "__main__":
-    version = "4.0.9"
+    version = "4.0.10"
 
     futures: List[concurrent.futures.Future] = []
 
